985acd88dccf1d18bfe799bcd40532211f44cbdc
monikagawryluk <monika.gawryluk6@gmail.com>
2022-08-30 23:28:46 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+//// Variables
+
+//// Functions
+
+//// Event Listeners
+
+const state = [
+  {
+    id: 1,
+    company: 'Photosnap',
+    logo: './images/photosnap.svg',
+    new: true,
+    featured: true,
+    position: 'Senior Frontend Developer',
+    role: 'Frontend',
+    level: 'Senior',
+    postedAt: '1d ago',
+    contract: 'Full Time',
+    location: 'USA Only',
+    languages: ['HTML', 'CSS', 'JavaScript'],
+    tools: [],
+  },
+];
+
+// const tagsContainer = document.querySelector('.tags-box');
+
+// const generateMarkupLanguage = function (lang) {
+//   return `<div class="tag-lang">${lang}</div>`;
+// };
+
+// const markupLanguage = state[0].languages
+//   .map(lang => generateMarkupLanguage(lang))
+//   .join('\n');
+
+// console.log(markupLanguage);
+
+// const markup = `
+//     <div class="tag-role">Frontend</div>
+//     <div class="tag-level">Senior</div>
+//   `;
+
+// const markupFull = `${markup} ${markupLanguage}`;
+// console.log(markupFull);
+
+// const generateTags = function () {
+//   tagsContainer.innerHTML = '';
+//   tagsContainer.insertAdjacentHTML('beforeend', markupFull);
+// };
+
+// generateTags();
